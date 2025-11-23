a62050eac08bbebaa32e663a41eb513dc308cd91
Lo <w4lo@ieng6-ece-19.ucsd.edu>
2025-11-23 15:44:49 -0800

    Core Design

---
+++ core.v
@@ -0,0 +1,65 @@
+module core  (
+	input clk,
+    input reset,
+	input [33:0] inst,
+	output ofifo_valid,
+    input [bw*row-1 : 0 ]  D_xmem, 
+    output [psum_bw*col-1 : 0 ] sfp_out
+); 
+    parameter bw  = 8;
+    parameter col = 8;
+    parameter psum_bw = 16;
+    parameter row = 4;
+
+
+wire [31:0] Q_xmem;
+wire l0_or_ififo;
+sram_32b_w2048 #(
+    .num(2048)
+) input_mem(
+    .CLK(clk),
+    .WEN(inst[18]),
+    .CEN(inst[19]),
+    .D(D_xmem),
+    .A(inst[17:7]),
+    .Q(Q_xmem)
+);
+
+wire [127:0] D_pmem, Q_pmem;
+genvar i;
+for(i = 0; i < 4; i = i + 1)begin
+    sram_32b_w2048 #(
+    .num(2048)
+    ) partial_mem(
+        .CLK(clk),
+        .WEN(inst[31]),
+        .CEN(inst[32]),
+        .D(D_pmem[(i+1) * 32 - 1: i*32]),
+        .A(inst[30:20]),
+        .Q(Q_pmem[(i+1) * 32 - 1: i*32])
+    );
+end
+
+
+
+corelet #(
+
+) corelet_inst(
+    .clk(clk),
+    .reset(reset),
+    .inst(inst[1:0]), // 1st bit : execute  0th: Load Kernel
+    .l0_or_ififo(inst[17]),
+    .l0_rd(inst[3]),
+    .l0_wr(inst[2]),
+    .in_data(Q_xmem),
+    .psum_accum_in(Q_pmem),
+    .accum(inst[33]), 
+    .ofifo_rd(inst[6]),
+    .ofifo_valid(ofifo_valid),
+    .psum_out(D_pmem),
+    .out_data(sfp_out)
+
+);
+
+
+endmodule
---
+++ corelet.v
@@ -0,0 +1,78 @@
+module corelet(
+    input clk,
+    input reset,
+    input [1 : 0] inst, // 1st bit : execute 0th: Load Kernel
+    input l0_or_ififo,
+    input l0_rd,
+    input l0_wr,
+    input [bw * row - 1 : 0] in_data,
+    input [127:0]psum_accum_in,
+    input accum,
+    input ofifo_rd,
+    output ofifo_valid,
+    output [127:0]psum_out,
+    output [psum_bw * col - 1 : 0] out_data
+);
+    parameter bw = 4;
+    parameter psum_bw = 16;
+    parameter col = 8;
+    parameter row = 8;
+
+    //MAC
+    wire [psum_bw * col - 1 : 0]mac_out;
+    wire [col - 1 : 0] mac_out_valid;
+    wire [bw * col - 1 : 0] mac_in_n, mac_in_w;
+    // assign mac_in_n = (inst[0])? l0_out : 0;
+    // assign mac_in_w = (inst[1])? l0_out : 0;
+    assign mac_in_w = l0_out;
+    //L0 FIFO
+    wire [bw * row - 1 : 0] l0_out;
+    
+    //OFIFO
+    wire [psum_bw * col - 1 : 0] ofifo_out;
+
+    //N
+
+    mac_array #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) mac_array_inst(
+        .clk(clk), 
+        .reset(reset),
+        .out_s(mac_out),
+        .in_w(mac_in_w),
+        .in_n(128'b0),
+        .inst_w(inst[1:0]),
+        .valid(mac_out_valid)
+        );
+    l0 #(.bw(bw)) l0_inst (
+        .clk(clk),
+        .reset(reset), 
+        .in(in_data), 
+        .out(l0_out), 
+        .rd(l0_rd),
+        .wr(l0_wr), 
+        .o_full(), 
+        .o_ready()
+        );
+    ofifo #(.col(col), .psum_bw(psum_bw)) ofifo_inst(
+        .clk(clk),
+        .reset(reset),
+        .in(mac_out),
+        .out(psum_out),
+        .rd(ofifo_rd),
+        .wr(mac_out_valid),
+        .o_full(),
+        .o_ready(),
+        .o_valid(ofifo_valid)
+        );
+genvar i;
+for (i = 0; i < col; i = i + 1)begin
+    sfu #(.bw(bw), .psum_bw(psum_bw)) sfu_inst(
+        .clk(clk),
+        .rst(reset),
+        .in_valid(accum), // When valid == 1, data transfer into SFU. 
+        .in(psum_accum_in[psum_bw * (i + 1) - 1 : psum_bw * i]),
+        .out(out_data[psum_bw * (i + 1) - 1 : psum_bw * i])
+    );
+end
+
+
+endmodule
