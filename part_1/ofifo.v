98555db8dea7f40b6df44548a6a82bdfff600bb8
Josephood7 <144367420+Josephood7@users.noreply.github.com>
2025-12-01 20:19:58 -0800

    synthesizable 36 depth fifo

--- part_1/ofifo.v
+++ part_1/ofifo.v
@@ -27,7 +27,7 @@ module ofifo (clk, in, out, rd, wr, o_full, reset, o_ready, o_valid);
 
   generate
 	  for (i=0; i<col ; i=i+1) begin : col_num
-			fifo_depth64 #(.bw(psum_bw)) fifo_instance (
+			fifo_depth36 #(.bw(psum_bw)) fifo_instance (
 			  .rd_clk(clk),
 			  .wr_clk(clk),
 			  .rd(rd_en),
