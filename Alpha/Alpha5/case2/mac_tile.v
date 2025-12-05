31b5b3af6e0fce3ef12662e30221185a51fec3ea
Wen-Chieh-Lo <w4lo@ucsd.edu>
2025-12-04 22:02:37 -0800

    Add files via upload

---
+++ Alpha/Alpha5/case2/mac_tile.v
@@ -0,0 +1,80 @@
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module mac_tile (clk, out_s, in_w, out_e, in_n, inst_w, inst_e, reset, zero_w, zero_e);
+
+parameter bw = 4;
+parameter psum_bw = 16;
+
+output [psum_bw-1:0] out_s;
+input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+output [bw-1:0] out_e; 
+input  [1:0] inst_w;
+output [1:0] inst_e;
+input  [psum_bw-1:0] in_n;
+input  clk;
+input  reset;
+input  zero_w;
+output zero_e;
+
+reg [1:0]inst_q;
+reg [bw-1:0]a_q;
+reg [bw-1:0]a_fwd_q;
+reg [bw-1:0]b_q;
+reg [psum_bw-1:0]c_q;
+reg [psum_bw-1:0]c_fwd_q;
+reg load_ready_q;
+reg zero, zero_all, zero_q;
+wire [psum_bw - 1 : 0] mac_out;
+
+assign out_e = a_fwd_q;
+assign inst_e = inst_q;
+assign out_s = zero_all? c_fwd_q : mac_out;
+assign zero_e = zero_q;
+
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
+        .a(a_q), 
+        .b(b_q),
+        .c(c_q),
+	.out(mac_out),
+        .zero(zero_all)
+); 
+
+always @(posedge clk)begin
+        if(reset)begin
+                inst_q <= 0;
+                load_ready_q <= 1;
+                a_q <= 0;
+                a_fwd_q <= 0;
+                b_q <= 0;
+                c_q <= 0;
+                c_fwd_q <= 0;
+                zero <= 0;
+                zero_all <= 0;
+                zero_q <= 0;
+        end
+        else begin
+                if(|inst_w)begin
+                        a_fwd_q <= in_w;
+                end
+
+                if(inst_w[0] && load_ready_q)begin
+                        b_q <= in_w;
+                        zero <= zero_w;
+                        load_ready_q <= 0;
+                end
+
+                if(!load_ready_q)begin
+                        inst_q[0] <= inst_w[0];
+                end
+                inst_q[1] <= inst_w[1];
+                zero_all <= zero ||zero_w;
+                zero_q <= zero_w;
+                c_fwd_q <= in_n;
+                if ((!(zero || zero_w)) && inst_w[1]) begin
+                        a_q <= in_w;
+                        c_q <= in_n;
+                end
+        end
+end
+
+endmodule
